4e6626abd33195689e10fc10571739cb00d512fe
Mark Williams <mark.williams@enable.com>
2019-12-01 20:11:15 +0000

    Going to need a little more fuel

--- day1.py
+++ day1.py
@@ -8,10 +8,18 @@ inputFile = open("day1input.txt","r")
 input = inputFile.readlines()
 inputFile.close()
 
-result = 0
+fuelMass = 0
+payloadMass = 0
 
 for line in input:
     moduleMass = int(line.rstrip())
-    result += FuelRequired(moduleMass)
+    payloadMass += moduleMass
+    fuelMass += FuelRequired(moduleMass)
 
-print result
+print fuelMass
+
+specificImpulse = 243 # equal to that of a Saturn V first stage
+deltaV = 9.81 * specificImpulse * np.log((fuelMass + payloadMass) / payloadMass)
+print deltaV
+
+# Delta-v comes to just under 1000 m/s. For comparison, around 9400 m/s is needed just to reach Earth orbit.
