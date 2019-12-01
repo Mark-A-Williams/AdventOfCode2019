d40c5caade05396029e0574357717c80096d44e1
Mark Williams <mark.williams@enable.com>
2019-12-01 13:23:49 +0000

    Complete Day 1 Part 1

--- day1.py
+++ day1.py
@@ -1,6 +1,16 @@
 import numpy as np
 
+def FuelRequired(mass):
+    return np.floor(mass/3)-2
+
 inputFile = open("day1input.txt","r")
 input = inputFile.readlines()
 inputFile.close()
 
+result = 0
+
+for i in range(len(input)):
+    moduleMass = int(input[i].rstrip())
+    result += FuelRequired(moduleMass)
+
+print result
