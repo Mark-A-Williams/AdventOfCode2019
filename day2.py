2ca92be906ccc577384b2318f39864825e24daae
Mark Williams <mark.williams@enable.com>
2019-12-02 23:25:05 +0000

    Complete Day 2 Part 2

--- day2.py
+++ day2.py
@@ -1,22 +1,52 @@
 import numpy as np
 
 def Compute(startIndex, list):
-    global programRunning
-    opcode = list[startIndex]
+    if(startIndex >= len(list)):
+        print 'problem1'
+        return False, list
+
+    opcode = list[startIndex]    
     index1, index2 = list[startIndex + 1], list[startIndex + 2]
     input1, input2 = list[index1], list[index2]
     outputIndex = list[startIndex + 3]
 
     if(opcode == 1):
         list[outputIndex] = input1 + input2
+        return True, list
 
     if(opcode == 2):
         list[outputIndex] = input1 * input2
+        return True, list
 
     if(opcode == 99):
-        print('Program complete. Value at position 0 is {}'.format(list[0]))
-        programRunning = False
+        return False, list
 
+    else:
+        print 'problem2'
+        print opcode
+        return False, list
+
+def IntcodeComputer(param1, param2, list):
+    list[1], list[2] = param1, param2
+
+    programRunning = True
+    startIndex = 0
+    while(programRunning):
+        programRunning, list = Compute(startIndex, list)
+        startIndex += 4
+
+    return list[0]
+
+def FindInputs(desiredResult, acceptableRange):
+    global input
+    for i in range(acceptableRange):
+        for j in range(acceptableRange):
+            list = input[:]
+            result = IntcodeComputer(i, j, list)
+            if(result == desiredResult):
+                return i, j
+
+# Main
 
 inputFile = open("day2input.txt","r")
 input = inputFile.read().split(",")
@@ -25,10 +55,6 @@ inputFile.close()
 for i in range(len(input)):
     input[i] = int(input[i])
 
-input[1], input[2] = 12, 2
+noun, verb = FindInputs(19690720, len(input))
 
-programRunning = True
-startIndex = 0
-while(programRunning):
-    Compute(startIndex, input)
-    startIndex += 4
+print 100 * noun + verb
