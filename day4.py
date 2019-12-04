99054d175e5485fdf237b923a570d04732cd3fbf
Mark Williams <mark.williams@enable.com>
2019-12-04 22:25:41 +0000

    Kill me. Complete Day 4.

--- day4.py
+++ day4.py
@@ -27,3 +27,36 @@ for n in range (a, b + 1):
     l(n)
 
 print len(k)
+
+# part 2
+
+def o(p):
+    q = str(p)
+    for r in range(1, len(q) - 2):
+        if (q[r] == q[r+1]):
+            if (q[r] != q[r + 2]) and (q[r] != q[r - 1]):
+                return True
+    if (q[-1] == q[-2]) and (q[-2] != q[-3]):
+        return True
+    if (q[0] == q[1]) and (q[1] != q[2]):
+        return True
+    return False
+    # s = len(q) - 2
+    # if ((q[s] == q[s + 1])):# and (q[s] != q[s - 1])):
+    #     return True
+
+t = []
+
+def u(v):
+    global t
+    if(o(v) and g(v)):
+        t.append(v)
+
+for w in range (a, b + 1):
+    u(w)
+
+print len(t)
+print t
+
+print o(677789)
+print g(677789)
