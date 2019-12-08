974d4cf39b600ca8789fafa9ab91d8c0cc48761e
Mark Williams <mark.williams@enable.com>
2019-12-08 22:21:06 +0000

    Complete Day 8 Part 2

--- day8.py
+++ day8.py
@@ -5,6 +5,15 @@ def CountNumbers(index):
     layer = input[startPixel: startPixel + numPixels]
     return layer.count("0"), layer.count("1"), layer.count("2")
 
+def FindPixelColour(pixel):
+    global input, numLayers
+
+    for i in range(numLayers):
+        pixelValue = int(input[numPixels * i + pixel])
+        if (pixelValue != 2):
+            return pixelValue
+    return 2
+
 # main
 
 inputFile = open("day8input.txt", "r")
@@ -25,3 +34,15 @@ for i in range(numLayers):
 print layerWithFewestZeros
 zeros, ones, twos = CountNumbers(layerWithFewestZeros)
 print ones * twos
+
+# part 2
+
+image = [2] * numPixels
+
+for i in range(len(image)):
+    image[i] = FindPixelColour(i)
+
+for i in range(6):
+    print image[ 25*i : 25*(i+1) ]
+
+# then employee the very sophisticated method of "squint a bit and try and make out words"
