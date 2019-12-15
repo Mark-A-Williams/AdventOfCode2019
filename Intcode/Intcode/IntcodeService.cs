81114e512a30645609eecb92c87d909cc5b186c5
Mark Williams <mark.williams@enable.com>
2019-12-15 21:18:21 +0000

    Barebones console app for tackling Intcode stuff

---
+++ Intcode/Intcode/IntcodeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Intcode
+{
+    public class IntcodeService
+    {
+        public IntcodeService()
+        {
+
+        }
+
+        public void PrintStuff()
+        {
+            Console.WriteLine("Hello, world!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Intcode/Intcode/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Intcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntcodeService _intcodeService = new IntcodeService();
+            _intcodeService.PrintStuff();
+        }
+    }
+}
